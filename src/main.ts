ea836ad2d85df35f404cce91f54bb3a0e2d98f55
AlejandroGaGa <alejandrogarciagallardo1234@gmail.com>
2025-02-19 16:44:30 -0600

    [serverless-framework] - ci serverless yml

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { configure } from '@codegenie/serverless-express';
+
+let server: Handler; //Lambda server instance
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.init(); // Initialize the NestJS application
+
+  // Get the underlying Express app instance from the NestJS HTTP adapter
+  const expressHandler = app.getHttpAdapter().getInstance();
+
+  // Configure serverless-express with the Express app instance
+  return configure({ app: expressHandler });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return await server(event, context, callback);
+};

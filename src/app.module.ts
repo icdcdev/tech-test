9920c4ab9638579ae0f9d0bfe48d01d335757f35
AlejandroGaGa <alejandrogarciagallardo1234@gmail.com>
2025-02-19 19:06:01 -0600

    [datasource] - fix of pg and datasource typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [ClientsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
